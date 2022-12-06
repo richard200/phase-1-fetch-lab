60746ddf604d3ea2d2350e1c62edcf32868305ec
richard200 <richmn200@gmail.com>
2022-12-06 12:18:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +21,4 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
