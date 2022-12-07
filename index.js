f4f61dcc9873d8493f2e44d9973a8a0dc8afdd4c
richard200 <richmn200@gmail.com>
2022-12-07 12:01:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  let returnFetch = fetch("https://anapioficeandfire.com/api/books")
   .then(res => res.json())
   .then(data => renderBooks(data))
+  return returnFetch;
+ 
+
 
 }
 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(books => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = books.name;
     main.appendChild(h2);
   });
+  
 }
 
 
